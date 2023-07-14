7dba0a3bb991219bb0d5625523009ee76c24a25e
Deemou <yepchani@gmail.com>
2023-07-14 19:45:30 +0900

    page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from './routes/notfound';
+import Home from './routes/home';
+import Preview from './routes/preview';
+import Submit from './routes/submit';
+import '@styles/style.scss';
+import '@styles/app.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="wrapper">
+        <Routes>
+          <Route path="*" element={<NotFound />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/preview" element={<Preview />}></Route>
+          <Route path="/submit" element={<Submit />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRefresh()],
   server: {
     open: true
   },
@@ -16,5 +17,14 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: 'src/setupTests',
     mockReset: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles')
+    }
   }
 });
