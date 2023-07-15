3c42e07896316e38370282d709df68cd4a7fb55d
Deemou <yepchani@gmail.com>
2023-07-16 04:00:36 +0900

    refactor: seperate layout component

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,16 +1,17 @@
 import Header from '@/components/header';
 import FormInfo from '@/components/form-info';
 import FormContent from '@/components/form-content';
+import Layout from '@/components/layout';
 import '@styles/form.scss';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="wrapper">
+      <Layout>
         <FormInfo />
         <FormContent />
-      </div>
+      </Layout>
     </>
   );
 }
