2cff76a3db49d884467b3a5a37d48878df986e20
Deemou <yepchani@gmail.com>
2023-07-16 03:09:07 +0900

    refactor: separate x-icon

--- src/components/remove-option-button.tsx
+++ src/components/remove-option-button.tsx
@@ -1,3 +1,5 @@
+import XIcon from '@/asset/icon/x-icon';
+
 interface RemoveOptionButtonProps {
   onClickRemove: () => void;
 }
@@ -7,18 +9,7 @@ export default function RemoveOptionButton({
 }: RemoveOptionButtonProps) {
   return (
     <button onClick={onClickRemove} className="remove-option-btn">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill="#5f6368"
-          d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
-        ></path>
-        <path d="M0 0h24v24H0z" fill="none"></path>
-      </svg>
+      <XIcon />
     </button>
   );
 }
