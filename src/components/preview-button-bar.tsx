67512371543b6683d8b99ccb078525219dffae86
Deemou <yepchani@gmail.com>
2023-07-16 21:20:27 +0900

    feat: add go-back-button to go home

--- src/components/preview-button-bar.tsx
+++ src/components/preview-button-bar.tsx
@@ -1,8 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '@/app/hooks';
 import { clearForm } from '@app/slices/contentSlice';
 
 export default function PreviewButtonBar() {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
+
+  const onClickGoBack = () => {
+    navigate('/');
+  };
 
   const onClickClearForm = () => {
     dispatch(clearForm());
@@ -11,6 +17,16 @@ export default function PreviewButtonBar() {
   return (
     <>
       <div aria-label="Preview button bar" className="preview-button-bar">
+        <div>
+          <button
+            onClick={onClickGoBack}
+            type="button"
+            aria-label="Go back"
+            className="go-back-button"
+          >
+            Go back
+          </button>
+        </div>
         <button
           onClick={onClickClearForm}
           type="button"
