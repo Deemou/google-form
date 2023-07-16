98c8089363d2c3f12b57f8b95a633d8e0672a371
Deemou <yepchani@gmail.com>
2023-07-16 21:22:16 +0900

    feat: add logic for form submit

--- src/components/preview-button-bar.tsx
+++ src/components/preview-button-bar.tsx
@@ -1,10 +1,31 @@
 import { useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '@/app/hooks';
-import { clearForm } from '@app/slices/contentSlice';
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import { updateErrorStatusAt, clearForm } from '@app/slices/contentSlice';
 
 export default function PreviewButtonBar() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const { questions } = useAppSelector((state) => state.contentSlice);
+
+  const onSubmit = () => {
+    let isError = false;
+    for (let i = 0; i < questions.length; i++) {
+      dispatch(updateErrorStatusAt({ index: i }));
+      if (
+        (questions[i].isRequired && questions[i].chosenOptions.length === 0) ||
+        questions[i].chosenOptions[0] === ''
+      ) {
+        isError = true;
+        break;
+      }
+    }
+
+    if (isError === true) {
+      window.alert('Please answer all the required questions!');
+      return;
+    }
+    navigate('/submit');
+  };
 
   const onClickGoBack = () => {
     navigate('/');
@@ -17,7 +38,15 @@ export default function PreviewButtonBar() {
   return (
     <>
       <div aria-label="Preview button bar" className="preview-button-bar">
-        <div>
+        <div className="left">
+          <button
+            onClick={onSubmit}
+            type="submit"
+            aria-label="Submit"
+            className="submit-button"
+          >
+            Submit
+          </button>
           <button
             onClick={onClickGoBack}
             type="button"
