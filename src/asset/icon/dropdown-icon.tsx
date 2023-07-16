1ea74f38777f0db92f14f8d6f5d60edbabcd5249
Deemou <yepchani@gmail.com>
2023-07-16 19:02:50 +0900

    refactor: separate dropdown-icon

---
+++ src/asset/icon/dropdown-icon.tsx
@@ -0,0 +1,26 @@
+export default function DropdownIcon() {
+  return (
+    <svg
+      width="20px"
+      height="24px"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000000"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {' '}
+        <rect x="0" fill="none" width="24" height="24"></rect>{' '}
+        <g>
+          {' '}
+          <path d="M7 10l5 5 5-5"></path>{' '}
+        </g>{' '}
+      </g>
+    </svg>
+  );
+}
--- src/components/checker.tsx
+++ src/components/checker.tsx
@@ -1,3 +1,4 @@
+import DropdownIcon from '@/asset/icon/dropdown-icon';
 import { questionType } from '@/types/formTypes';
 
 interface CheckerProps {
@@ -9,30 +10,7 @@ export default function Checker({ type }: CheckerProps) {
     <div className="checker">
       {type === 'radio' && <input type="radio" disabled />}
       {type === 'checkboxes' && <input type="checkbox" disabled />}
-      {type === 'dropdown' && (
-        <svg
-          width="20px"
-          height="24px"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="#000000"
-        >
-          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-          <g
-            id="SVGRepo_tracerCarrier"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></g>
-          <g id="SVGRepo_iconCarrier">
-            {' '}
-            <rect x="0" fill="none" width="24" height="24"></rect>{' '}
-            <g>
-              {' '}
-              <path d="M7 10l5 5 5-5"></path>{' '}
-            </g>{' '}
-          </g>
-        </svg>
-      )}
+      {type === 'dropdown' && <DropdownIcon />}
     </div>
   );
 }
